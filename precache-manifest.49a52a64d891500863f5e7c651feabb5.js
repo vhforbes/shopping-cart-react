71bd7e1812241ec016684a4976c2030807f531e4
Victor Forbes <vhforbes@gmail.com>
2020-10-28 16:12:11 -0300

    Updates

---
+++ precache-manifest.49a52a64d891500863f5e7c651feabb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a28d24406e4a9338a1a35f80799c8f67",
+    "url": "/shopping-cart-react/index.html"
+  },
+  {
+    "revision": "c39e054ed068852073ec",
+    "url": "/shopping-cart-react/static/css/main.3affb28f.chunk.css"
+  },
+  {
+    "revision": "99bf493f699123ed39d6",
+    "url": "/shopping-cart-react/static/js/2.b0cd1481.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-cart-react/static/js/2.b0cd1481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c39e054ed068852073ec",
+    "url": "/shopping-cart-react/static/js/main.0be1f1a0.chunk.js"
+  },
+  {
+    "revision": "fc53af84ab093c63cb89",
+    "url": "/shopping-cart-react/static/js/runtime-main.c367a05c.js"
+  }
+]);
